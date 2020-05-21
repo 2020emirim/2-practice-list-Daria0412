2e3609c233e4b5fdda2995f6e6d0e5467f2d29c1
daria0412 <s2018w29@icloud.com>
2020-05-21 10:36:02 +0900

    add detailView

---
+++ bookmark/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from bookmark.views import BookmarkList, BookmarkCreateView
+
+app_name = 'bookmark'
+
+urlpatterns = [
+    path('', BookmarkList.as_view(), name='list'),  # {% url bookmark:list %}
+    path('create/', BookmarkList.as_view(), name='list'),
+    path('add/', BookmarkCreateView.as_view(), name='add'),
+]
+
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView
 
-# Create your views here.
+from bookmark.models import Bookmark
+
+
+class BookmarkList(ListView):  # bookmark_list.html
+    model = Bookmark
+
+
+class BookmarkCreateView(CreateView):  # bookmark-form.html
+    model = Bookmark
+    fields = ['site_name', 'url'] #<form>
+    template_name_suffix = '_create'  # bookmark_create.html -->이름 바꾸기
+    success_url = reverse_lazy('bookmark:list') #db에 저장된 후에 보여주기
+
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
+    
