748dcdd63aca6a50ed225de42055b2685b27c556
daria0412 <s2018w29@icloud.com>
2020-05-22 15:31:16 +0900

    get_absolute_url() detail

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 class Bookmark(models.Model):
     site_name = models.CharField(max_length = 20)
     url = models.URLField('site URL')
@@ -7,3 +7,5 @@ class Bookmark(models.Model):
     def __str__(self):
         return "제목 : "+self.site_name+", URL : " + self.url
     
+    def get_absolute_url(self):
+        return reverse('bookmark:detail', kwargs={'pk':self.id})
--- bookmark/views.py
+++ bookmark/views.py
@@ -23,5 +23,5 @@ class BookmarkUpdateView(UpdateView):
     model = Bookmark
     fields = ['site_name', 'url']
     template_name_suffix = '_update'
-    success_url = reverse_lazy('bookmark:list')
+    #success_url = reverse_lazy('bookmark:list')
 
