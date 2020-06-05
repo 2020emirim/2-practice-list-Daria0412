84aa492ad3aef7618668f78bb06c778a3eca1b50
daria0412 <s2018w29@icloud.com>
2020-06-05 18:13:20 +0900

    friday pagination

---
+++ README.md
@@ -0,0 +1,26 @@
+# Practice_List
+## bookmark
+- github 소스 -> PyCharm 프로젝트
+- bookmark 앱 만들기
+- models Bookmark 만들기
+- admin Bookmark 등록하기
+- python manage.py makemigations bookmark
+- python manage.py migrate
+- python manage.py createsuperuser
+- models Bookmark __str__() 출력하는 문구 설정
+- urls bookmark:list
+- views BookmarkList
+- templates bookmark_list.html
+- views BookmarkCreateView
+- templates bookamrk_create
+- urls add
+- modify bookmark_list Add Bookmark link
+- views bookmarkDetailView
+- templates bookmark_detail
+- urls detail/<int:pk>/
+- modify bookmark_list detail link
+- update : views BookmarkUpdateView, templates bookmark_update.html, urls, modify Modify link
+- delete : views BookmarkDeleteView, templates bookmark_delete.html, urls, modify Delete link
+- Cancel button 만들기, base.html 확장하기, bootstrap 적용하기
+- pagination views, base.html, bookmark_list.html
+- static CSS, ...
--- bookmark/views.py
+++ bookmark/views.py
@@ -7,24 +7,25 @@ from bookmark.models import Bookmark
 
 class BookmarkList(ListView):  # bookmark_list.html
     model = Bookmark
+    paginate_by = 3
 
 
 class BookmarkCreateView(CreateView):  # bookmark-form.html
     model = Bookmark
-    fields = ['site_name', 'url'] #<form>
-    template_name_suffix = '_create'  # bookmark_create.html -->이름 바꾸기
-    success_url = reverse_lazy('bookmark:list') #db에 저장된 후에 보여주기
+    fields = ['site_name', 'url']  # <form>태그엔 fields가 들어가야됨.
+    template_name_suffix = '_create'  # bookmark_create.html
+    success_url = reverse_lazy('bookmark:list')  # DB에 저장된 후 늦게 호출해라.
 
 
 class BookmarkDetailView(DetailView):
     model = Bookmark
 
+
 class BookmarkUpdateView(UpdateView):
     model = Bookmark
-    fields = ['site_name', 'url']
-    template_name_suffix = '_update'
+    fields = ['site_name', 'url']  # <form>
+    template_name_suffix = '_update'  # bookmark_update.html
     success_url = reverse_lazy('bookmark:list')
-
 
 
 class BookmarkDeleteView(DeleteView):
